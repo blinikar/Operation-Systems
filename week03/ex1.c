4d13b3a70dde2afcd1be3ffd78914eda18f47c17
blinikar <blinov.egor2013@mail.ru>
2022-09-20 21:27:57 +0300

    week03

--- week03/ex1.c
+++ week03/ex1.c
@@ -11,23 +11,18 @@ int main(){
     const int x = 10;
     const int const *q = &x;
 
-    const int* const p = malloc(sizeof(int)*5);
-    memset(p, 10, sizeof(int)*5);
-
-    scanf("%d", &p);
-    scanf("%d", &p + sizeof(int));
-    scanf("%d", &p + 2 * sizeof(int));
-    scanf("%d", &p + 3 * sizeof(int));
-    scanf("%d", &p + 4 * sizeof(int));
-
-    for (int i = 0; i < 1; i++) {
-        const int const age = *(&p + i * sizeof(int));
-        int year = foo(age);
-        const int const *pointer = &p + i * sizeof(int);
-        pointer = &year; 
+    int* const p = (int*) malloc(sizeof(int)*5);
+    
+    for (int i = 0; i < 5; i++) {
+        *(p + i) = x;
+        printf("%p\n", p + i);
     }
 
-    free(p);
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", p + i);
+        *(p + 1) = foo(*(p + 1));
+    } 
 
+    free(p);
 	return EXIT_SUCCESS;
 }
