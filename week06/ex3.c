d853e723c822acafb730c2b166140257179aa832
blinikar <blinov.egor2013@mail.ru>
2022-10-08 20:10:22 +0300

    Assignment

--- week06/ex3.c
+++ week06/ex3.c
@@ -5,16 +5,19 @@ struct Process {
     int i;
     int arrivalTime;
     int burstTime;
+    int timeToComplete;
+
+    int waitingTime;
 };
 typedef struct Process Process;
 
 
-int comparator(const void * elem1, const void * elem2) {
-    Process f = *((Process*)elem1);
-    Process s = *((Process*)elem2);
-    if (f.arrivalTime > s.arrivalTime) return  1;
-    if (f.arrivalTime < s.arrivalTime) return -1;
-    return 0;
+void shiftQueue(int *q, int n) {
+    int firstItem = q[0];
+    for (int i = 1; i < n; i++) {
+        q[i-1] = q[i];
+    }
+    q[n-1] = firstItem;
 }
 
 int main(void) {
@@ -22,7 +25,7 @@ int main(void) {
     printf("Count of processes: ");
     scanf("%d", &n);
 
-    Process processes[n];
+    Process processes[n+1];
 
     for (int i = 0; i < n; i++) {
         processes[i].i = i;
@@ -30,34 +33,72 @@ int main(void) {
         scanf("%d", &processes[i].arrivalTime);
         printf("Burst time of process %d: ", i);
         scanf("%d", &processes[i].burstTime);
+        
+        processes[i].timeToComplete = processes[i].burstTime;
+        processes[i].waitingTime = 0;
+        
         printf("Added!\n");
     }
 
-    qsort(processes, n, sizeof(Process), comparator);
+    processes[n].i = n;
+    processes[n].arrivalTime = __INT_MAX__;
+    processes[n].timeToComplete = __INT_MAX__;
 
     int waitingSum = 0;
     int tatSum = 0;
 
-    int CTs[n];
-    for (int i = 0; i < n; i++) {
-        int burstTime = processes[i].burstTime;
-        int arrivalTime = processes[i].arrivalTime;
-        
-        if (i == 0 || processes[i].arrivalTime > CTs[i-1]) 
-            CTs[i] = processes[i].arrivalTime + processes[i].burstTime;
-        else 
-            CTs[i] = CTs[i-1] + processes[i].burstTime;
+    int timeQuantum = 3;
+    scanf("%d", &timeQuantum);
 
-        int waitingTime = CTs[i] - burstTime - arrivalTime;
-        int TAT = waitingTime + burstTime;
+    int queue[n];
+    int queueN = 0;
 
-        waitingSum += waitingTime;
-        tatSum += TAT;
+    int count = 0;
+    int inProgress = -1;
+    int execTime = 0;
+    for (int t = 0; count < n; t++) {
+        int smallest = n;
 
-        printf("=== Process [%d] ===\n", processes[i].i);
-        printf("Waiting time: %d\n", waitingTime);
-        printf("TAT: %d\n", TAT);
-        printf("CT: %d\n", CTs[i]);
+        for (int i = 0; i < n; i++) {
+            if (processes[i].arrivalTime == t) {
+                queue[queueN] = i;
+                queueN++;
+            }
+        }
+
+        if (queueN == 0) continue;
+
+        if (inProgress >= 0 && execTime < timeQuantum) smallest = inProgress;
+        else {
+            smallest = queue[0];
+            inProgress = queue[0];
+            execTime = 0;
+        }
+
+        processes[smallest].timeToComplete--;
+        execTime++;
+
+        if (execTime == timeQuantum) shiftQueue(queue, queueN);
+
+        if (processes[smallest].timeToComplete == 0) {
+            count++;
+            inProgress = -1;
+
+            shiftQueue(queue, queueN);
+            queueN--;
+
+            int CT = t+1;
+            int waitingTime = CT - processes[smallest].arrivalTime - processes[smallest].burstTime;
+            int TAT = CT - processes[smallest].arrivalTime; 
+
+            waitingSum += waitingTime;
+            tatSum += TAT;
+
+            printf("=== Process [%d] ===\n", processes[smallest].i);
+            printf("Waiting time: %d\n", waitingTime);
+            printf("TAT: %d\n", TAT);
+            printf("CT: %d\n", CT);
+        }
     }
 
     printf("=== Metrics ===\n");
