b07eda30f19ee3ad84e134dcda5b6142a845c9cb
blinikar <blinov.egor2013@mail.ru>
2022-10-25 20:53:04 +0300

    5 task fix

--- week08/ex5.c
+++ week08/ex5.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main () {
-    char **s;
+    char **s = malloc(sizeof(char*));
     char foo[] = "Hello World";
-    s = foo;
-    printf("s is %s\n", s);
-    s[0] = &foo;
+    *s = foo;
+    printf("s is %s\n", *s);
+    s[0] = foo;
     printf("s[0] is %s\n", s[0]);
-    return(0);
+    return 0;
 }
